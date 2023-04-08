8fe5d96fc76df4c526ea57dfccc66aafbcf8a4cf
EC2 Default User <ec2-user@ip-172-31-29-44.ap-northeast-1.compute.internal>
2023-04-08 11:11:13 +0000

    stopwatch

--- main.js
+++ main.js
@@ -8,11 +8,42 @@ var startTime;
 var elapsedTime = 0;
 var holdTime = 0;
 
-window.onload = function(){
+window.onload = function () {
 showTime = document.getElementById('time');
 startButton = document.getElementById('start');
 stopButton = document.getElementById('stop');
 resetButton = document.getElementById('reset');
+}
+
+　//スタートボタンがクリックされたら時間を進める//
+  function start(){
+  startTime = Date.now();
+  measureTime();
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  resetButton.disabled = false;
+}
+
+//ストップボタンがクリックされたら時間を止める//
+  function stop(){
+  clearInterval(timer)
+  holdTime += Date.now() - startTime;
+  
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = false;
+}
+
+//リセットボタンがクリックされたら時間を０に戻す//
+  function reset(){
+  clearInterval(timer)
+  elapsedTime = 0;
+  holdTime = 0;
+  showTime.textContent = "0:0:0:0"
+  
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton = true;
 }
 
 
@@ -26,38 +57,3 @@ function measureTime(){
   }, 10);
 }
   
-
-
-//スタートボタンがクリックされたら時間を進める//
-function clickbutton(start) {
-  console.log(start)
-  startTime = Date.now()
-  measureTime(
-  startButton.disabled = true;
-  stopButton.disabled = false;
-  resetButton.disabled = false;)
-}
-
-//ストップボタンがクリックされたら時間を止める//
-function clickbutton(stop) {
-  console.log(stop)
-  clearInterval(timer)
-  holdTime += Date.now() - startTime;
-  
-  startButton.disabled = false;
-  stopButton.disabled = true;
-  resetButton.disabled = false;
-}
-
-//リセットボタンがクリックされたら時間を０に戻す//
-function clickbutton(reset) {
-  console.log(reset)
-  clearInterval(timer)
-  elapsedTime = 0;
-  holdTime = 0;
-  showTime.textContent = "0:0:0:0"
-  
-  startButton.disabled = false;
-  stopButton.disabled = true;
-  resetButton = true;
-}
